21e5a3b4ac94dad459ff6ea1722567bf7a25f540
Alexxsaz <alexxsaz96@gmail.com>
2025-01-20 19:04:26 +0500

    Added new verification to logic settings provider

--- TagCloud/Infrastructure/Providers/LogicSettingsProvider.cs
+++ TagCloud/Infrastructure/Providers/LogicSettingsProvider.cs
@@ -14,15 +14,23 @@ public class LogicSettingsProvider : ILogicSettingsProvider
         logicSettings = currentLogicSettings
             .AsResult()
             .Then(CheckRadiusStepValue)
-            .Then(CheckAngleStepValue);
+            .Then(CheckAngleStepValue)
+            .Then(CheckPointGeneratorType);
 
     private static Result<LogicSettings> CheckRadiusStepValue(LogicSettings currentLogicSettings) =>
         currentLogicSettings.RadiusStep <= 0
-            ? Result.Fail<LogicSettings>($"{nameof(currentLogicSettings.RadiusStep)} must be more than zero")
+            ? Result.Fail<LogicSettings>($"{nameof(currentLogicSettings.RadiusStep)} should be greater than zero")
             : currentLogicSettings.AsResult();
 
     private static Result<LogicSettings> CheckAngleStepValue(LogicSettings currentLogicSettings) =>
         currentLogicSettings.AngleStep <= 0
-            ? Result.Fail<LogicSettings>($"{nameof(currentLogicSettings.AngleStep)} must be more than zero")
+            ? Result.Fail<LogicSettings>($"{nameof(currentLogicSettings.AngleStep)} should be greater than zero")
             : currentLogicSettings.AsResult();
+
+    private static Result<LogicSettings> CheckPointGeneratorType(LogicSettings currentLogicSettings) =>
+        Enum.IsDefined(typeof(PointGeneratorType), currentLogicSettings.PointGeneratorType)
+            ? currentLogicSettings.AsResult()
+            : Result.Fail<LogicSettings>($"Unknown point generator type.\n" +
+                                         $"Point generator type should be one of following types: " +
+                                         $"{Enum.GetValues(typeof(PointGeneratorType))}");
 }
